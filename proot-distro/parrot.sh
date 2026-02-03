4aa09d3c168b45a0ab1ea53101c69eb441b5c04f
aniketchavan2211 <aniketchavan2211@gmail.com>
2026-02-03 11:59:21 +0530

    added: parrotsec shell script for proot-distro

---
+++ proot-distro/parrot.sh
@@ -0,0 +1,7 @@
+# Custom Parrot Security distro
+
+DISTRO_NAME="parrotsec"
+DISTRO_COMMENT="Parrot Security arm64 ( aarch64 )"
+
+TARBALL_URL['arm64']="https://github.com/risecid/AndronixOrigin/blob/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz"
+TARBALL_SHA256['arm64']="6bbcff4337fa36b2bdcdd8929e315a814803f351c702c2cec3bfab1682fa28e3"
