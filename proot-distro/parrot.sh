51f0ec53392e76f713580ac3a924e493936e8de7
aniketchavan2211 <aniketchavan2211@gmail.com>
2026-02-03 12:32:50 +0530

    error: need to fix integrity check

--- proot-distro/parrot.sh
+++ proot-distro/parrot.sh
@@ -1,7 +1,7 @@
 # Custom Parrot Security distro
 
-DISTRO_NAME="parrotsec"
+DISTRO_NAME="Parrot Security"
 DISTRO_COMMENT="Parrot Security arm64 ( aarch64 )"
 
-TARBALL_URL['arm64']="https://github.com/risecid/AndronixOrigin/blob/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz"
-TARBALL_SHA256['arm64']="6bbcff4337fa36b2bdcdd8929e315a814803f351c702c2cec3bfab1682fa28e3"
+TARBALL_URL['aarch64']="https://github.com/aniketchavan2211/OSes-And-Tools/blob/master/proot-distro/parrot-rootfs-arm64.tar.xz"
+TARBALL_SHA256['aarch64']="6bbcff4337fa36b2bdcdd8929e315a814803f351c702c2cec3bfab1682fa28e3"
